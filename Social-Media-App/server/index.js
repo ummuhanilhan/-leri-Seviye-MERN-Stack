cd6130cccfc838f65f458617b9648f226dc9b52f
ummuhanilhan <ummuhan.ilhan@bil.omu.edu.tr>
2022-10-18 14:16:22 +0300

    Bismillahirrahanirrahim

---
+++ Social-Media-App/server/index.js
@@ -0,0 +1,14 @@
+
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app=express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}))
+
+app.use(cors())
